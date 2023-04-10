43d39510f54920b348829a65db4cbb509c719e76
maisha-maliha <officiallymaisha@gmail.com>
2023-04-10 14:45:43 +0600

    got form data in JSON form

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+let profileInfo = '';
 let server = http.createServer((req, res)=>{
     if(req.url == '/'){
         res.writeHead(200,{'Content-Type':'text/html'});
@@ -17,8 +18,18 @@ let server = http.createServer((req, res)=>{
     }
     if(req.method ==='POST'){
         req.on('data', chunk =>{
-            let resdata = chunk.toString();
-            console.log(resdata);
+            let data, profileObj;
+            data = chunk.toString();
+            profileInfo = data.split("&");
+            data ='';
+            profileObj ='{';
+            profileInfo.forEach(element => {
+                data = element.split("=");
+                profileObj += `"${data[0]}" : "${data[1]}",`;
+            });
+            profileObj[profileObj.length-1] = " ";
+            profileObj += '"nothing":"nothing"}';
+            console.log(JSON.parse(profileObj));
             res.end();
         });
     }
